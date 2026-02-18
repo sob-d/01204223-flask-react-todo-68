a840835f365bf1107f172f46a006b2b5623a29b5
Krittiphong Praichuen <krittiphong@biggiers-MacBook-Air.local>
2026-02-18 18:46:43 +0700

    Split Component Todo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TodoItem from './TodoItem.jsx'
 
 function App() {
   const TODOLIST_API_URL = 'http://127.0.0.1:5000/api/todos/';
@@ -75,7 +76,7 @@ function App() {
     }
   }
 
-    async function addNewComment(todoId) {
+  async function addNewComment(todoId, newComment) {     // เพิ่ม parameter
     try {
       const url = `${TODOLIST_API_URL}${todoId}/comments/`;
       const response = await fetch(url, {
@@ -83,10 +84,12 @@ function App() {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ 'message': newComments[todoId] || "" }),
+        body: JSON.stringify({ 'message': newComment }),    // ใช้ newComment
       });
       if (response.ok) {
-        setNewComments({ ...newComments, [todoId]: "" });
+        // 
+        // ******  ลบบรรทัด setNewComments({ ...newComments, [todoId]: "" }); *******
+        // 
         await fetchTodoList();
       }
     } catch (error) {
@@ -99,34 +102,13 @@ function App() {
       <h1>Todo List</h1>
       <ul>
         {todoList.map(todo => (
-          <li key={todo.id}>
-            <span className={todo.done ? "done" : ""}>{todo.title}</span>
-            <button onClick={() => {toggleDone(todo.id)}}>Toggle</button>
-            <button onClick={() => {deleteTodo(todo.id)}}>❌</button>
-
-            {(todo.comments) && (todo.comments.length > 0) && (
-              <>
-                <b>Comments:</b>
-                <ul>
-                  {todo.comments.map(comment => (
-                    <li key={comment.id}>{comment.message}</li>
-                  ))}
-                </ul>
-              </>
-            )}
-            <div className="new-comment-forms">
-              <input
-                type="text"
-                value={newComments[todo.id] || ""}
-                onChange={(e) => {
-                  const value = e.target.value;
-                  setNewComments({ ...newComments, [todo.id]: value });
-                }}
-              />
-
-              <button onClick={() => {addNewComment(todo.id)}}>Add Comment</button>
-            </div>
-          </li>
+          <TodoItem 
+            key={todo.id} 
+            todo={todo}
+            toggleDone={toggleDone}
+            deleteTodo={deleteTodo}
+            addNewComment={addNewComment}
+          />
         ))}
       </ul>
       New: <input type="text" value={newTitle} onChange={(e) => {setNewTitle(e.target.value)}} />
